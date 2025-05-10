0e719c01d74e500e0fd1096e16fb0c477046b1f8
MahiKhan5360 <86805135+MahiKhan5360@users.noreply.github.com>
2025-05-10 23:41:46 +0600

    Update model.py

--- model.py
+++ model.py
@@ -74,5 +74,21 @@ class ResNet(nn.Module):
             layers.append(block(self.in_channels, out_channels))
 
         return nn.Sequential(*layers)
+def forward(self, x):
+        x = self.relu(self.bn1(self.conv1(x)))
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+
+        return x
+
+
 
     
